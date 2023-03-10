05f42b8cc932c745577b42c02f0f6c6d7746d0c4
MohanKumar-2003 <siva.mohan.kumar.2003@gmail.com>
2023-03-10 15:59:40 +0530

    path delete commit

--- ecommerce-server/server.js
+++ ecommerce-server/server.js
@@ -35,12 +35,6 @@ mongoose.connect(URLI,{
     if(err) throw err;
     console.log('Connected')
 })
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'))
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(_dirname,'client','build','index.html'))
-    })
-}
 const PORT=process.env.port || 5000
 app.listen(PORT, ()=>{
     console.log('Server is running on port', PORT)
